ffd4f0e5e887891bc324e0856b0d490a25c61ad9
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-09-25 15:12:51 -0600

    cleanup up and writing boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+
+import { Box, css } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Box css={wrapperCSS}>Hello World</Box>
   );
 }
+
+const wrapperCSS = css`
+  border: 2px solid red;
+`
 
 export default App;
